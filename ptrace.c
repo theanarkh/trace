0e32656bc689f35d831450824eaf5ffcf382f8dd
theanarkh <2923878201@qq.com>
2021-12-02 00:39:38 +0800

    Create ptrace.c
    use ptrace to find all system call that call by certain process

---
+++ ptrace.c
@@ -0,0 +1,53 @@
+/*
+  use ptrace to find all system call that call by certain process
+*/
+
+#include <sys/ptrace.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/reg.h>
+
+int main(int argc, char *argv[]) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        printf("fork failed");
+        exit(-1);
+    } else if (pid == 0) {
+        // set state of child process to PTRACE
+        ptrace(PTRACE_TRACEME,0,NULL,NULL);
+        // child will change to stopped state when in execve call, then send the signal to parent
+        execve(argv[1], NULL, NULL);
+    } else {
+        int status;
+        int bit = 1;
+        long num;
+        long ret;
+        // wait for child
+        wait(&status);
+        if(WIFEXITED(status))
+            return 0;
+        // this is for execve call which will not return, and for os of 64-it => ORIG_RAX * 8 or os of 32-it => ORIG_EAX * 4
+        num = ptrace(PTRACE_PEEKUSER, pid, ORIG_RAX * 8, NULL);
+        printf("system call num = %ld\n", num);
+        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+        while(1) {
+            wait(&status);
+            if(WIFEXITED(status))
+                return 0;
+            // for enter system call
+            if(bit) {
+                num = ptrace(PTRACE_PEEKUSER, pid, ORIG_RAX * 8, NULL);
+                printf("system call num = %ld", num);
+                bit = 0;
+            } else { // for return of system call
+                ret = ptrace(PTRACE_PEEKUSER, pid, RAX*8, NULL);
+                printf("system call return = %ld \n", ret);
+                bit = 1;
+            }
+            // let this child process continue to run until call next system call
+            ptrace(PTRACE_SYSCALL,pid,NULL,NULL);
+        }
+    }
+}
